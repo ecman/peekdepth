7f3ec21926cccf33f84b774eaa58be51d0dbfa7b
ecman <ecman@users.noreply.github.com>
2017-04-30 09:39:34 -0700

    Write core function

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
 module.exports = peekdepth;
 
 function peekdepth(arry, seen) {
+  seen = seen || new WeakSet();
+  if (!(arry instanceof Array) ||
+      seen.has(arry)) {
+    return 0;
+  }
+  seen.add(arry);
+  let i = 0;
+  let depth = 1;
+  while (i < arry.length) {
+    depth += peekdepth(arry[i], seen);
+    i += 1;
+  }
+  return depth;
 }
