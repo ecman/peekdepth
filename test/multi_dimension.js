491a2831af3ef246e2de0ecf3a50bd8d750c2ecc
ecman <ecman@users.noreply.github.com>
2017-05-11 09:34:52 -0700

    Correct the multi-dimension test
    Core logic should not act as a tally

--- test/multi_dimension.js
+++ test/multi_dimension.js
@@ -5,10 +5,14 @@ const assert = require('assert');
 let someArray;
 let depth;
 
-someArray = [[[]]];
+someArray = [[[]], [[]]];
 depth = peekdepth(someArray);
 assert.equal(3, depth, `depth should be 3 not ${depth}`);
 
-someArray = [1, [2, [3, [[5], 4]]]];
+someArray = [1, [2, [3, [[5], 4]]], 1, [2, [3, [[5], 4]]]];
 depth = peekdepth(someArray);
 assert.equal(5, depth, `depth should be 5 not ${depth}`);
+
+someArray = [[1, [2], 3], [1, [2], 3]];
+depth = peekdepth(someArray);
+assert.equal(3, depth, `depth should be 3 not ${depth}`);
